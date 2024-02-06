e31dcc80fc559f7fceb9fd1a39745ef9160644b0
Kevin Li <kevin@kevins-mbp-4.dyndns.rice.edu>
2024-02-06 15:35:22 -0600

    added complexity

--- sketch_feb6b/sketch_feb6b.ino
+++ sketch_feb6b/sketch_feb6b.ino
@@ -1,11 +1,12 @@
-const int trigPin = A;
-const int echoPin = B;
+const int trigPin = 11;
+const int echoPin = 12;
 long duration;
 int distanceCm, distanceInch;
 
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(3, OUTPUT);
+  pinMode(5, OUTPUT);
   
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
@@ -14,6 +15,8 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
@@ -21,4 +24,27 @@ void loop() {
   digitalWrite(trigPin, LOW);
 
   duration = pulseIn(echoPin, HIGH);
+
+  distanceCm = duration * 1/58;
+  distanceInch = duration * 1/148;
+  Serial.print("Distance: ");
+  Serial.print(distanceCm);
+  Serial.print(" cm/");
+  Serial.print(distanceInch);
+  Serial.println(" in");
+  delay(1000);
+
+
+  if (distanceCm <10) {
+    digitalWrite(3, HIGH);
+    digitalWrite(5, LOW);
+    Serial.print("Distance is less than 10cm");
+    }
+  else {
+    digitalWrite(3, LOW);
+    digitalWrite(5, HIGH);  
+    Serial.print("Distance is more than 10cm");
+    }
+
+  
 }
